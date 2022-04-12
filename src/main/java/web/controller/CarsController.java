b9528dd48929ce9610c9901dc1a1075826c54651
Takhir Musakhanov <m.taha@mail.ru>
2022-04-12 20:14:52 +0400

    Работает

--- src/main/java/web/controller/CarsController.java
+++ src/main/java/web/controller/CarsController.java
@@ -4,20 +4,21 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import web.servis.CarsService;
 
 
 @Controller
-@RequestMapping()
 public class CarsController {
 
-    @Autowired
     private CarsService carsService;
+    @Autowired
+    public CarsController(CarsService carsService) {
+        this.carsService = carsService;
+    }
 
     @GetMapping(value = "/cars")
-    public String printWelcome(@RequestParam("count") int count, ModelMap model) {
+    public String printCars(@RequestParam(value = "count", required = false) String count, ModelMap model) {
         model.addAttribute("cars", carsService.getCars(count));
         return "cars";
     }
--- src/main/java/web/servis/CarsService.java
+++ src/main/java/web/servis/CarsService.java
@@ -15,14 +15,14 @@ public class CarsService {
         cars = new ArrayList<>();
         cars.add(new Car(++counter, "Volga", "yellow"));
         cars.add(new Car(++counter, "Pobeda","white"));
-        cars.add(new Car(++counter, "Сhayka", "black"));
+        cars.add(new Car(++counter, "Chayka", "black"));
         cars.add(new Car(++counter, "Lada", "red"));
         cars.add(new Car(++counter, "Moskvich", "blue"));
     }
 
-    public List<Car> getCars(int count) {
-        if (count < 5 && count > 0) {
-            return cars.subList(0, (count - 1));
+    public List<Car> getCars(String count) {
+        if (count != null && Integer.parseInt(count) < 5 && Integer.parseInt(count) > 0) {
+            return cars.subList(0, (Integer.parseInt(count) ));
         }
         return cars;
     }
